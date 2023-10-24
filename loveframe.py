b6684cdb927b20c6fbdbfbd45bad19741e461734
Annas Asif <muhammad.annas@ozitechnology.com>
2023-10-24 10:01:19 +0500

    new changes added in loveframe UI

--- loveframe.py
+++ loveframe.py
@@ -3,6 +3,9 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import * 
+from functools import partial
+
+from io import BytesIO
 
 import requests
 import json
@@ -17,10 +20,7 @@ class LoveFrame(QDialog):
         #Loading LogIn UI
         loadUi("loveframe.ui", self)
 
-        # Create a list of image paths
-        response = requests.get('http://161.97.164.28:8080/api/categories/seemore')
-        data = json.loads(response.text)
-        image_paths = data['content']
+        
 
         #Back Button
         self.back.clicked.connect(self.to_mainscreen)
@@ -28,41 +28,13 @@ class LoveFrame(QDialog):
         self.back.enterEvent = self.button_entered6
         self.back.leaveEvent = self.button_left6
 
-        layout = QVBoxLayout(self)
-        frame = QFrame()
-        frame.setFixedSize(200, 200)
+        self.widget = self.findChild(QWidget, "widget")
+        self.widget_2 = self.findChild(QWidget, "widget_2")
+        
+        self.image_widget = self.findChild(QWidget, "image_widget")
 
-        scroll_area = QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        scroll_widget = QWidget()
-        scroll_area.setWidget(scroll_widget)
-
-        scroll_layout = QVBoxLayout(scroll_widget)
-        scroll_layout.setContentsMargins(15, 15, 15, 15)
-
-        # Add labels or other widgets to the scroll layout
-        for i in image_paths:
-            button= QPushButton(i['name'], self)
-            button.setStyleSheet("""
-                QPushButton {
-                    border-radius:15%;
-                    font: 12pt 'MS Shell Dlg 2';
-                    color: rgb(255, 255, 255);
-                    background-color:  qlineargradient(spread:pad, x1:0.211091, y1:0.199, x2:0.608, y2:0.603, stop:0.767045 rgba(30, 63, 102, 255), stop:0.977273 rgba(34, 70, 117, 255));
-                    color: white;
-                }
-                QPushButton:hover {
-                    border-radius:15%;
-                    font: 12pt 'MS Shell Dlg 2';
-                    color: rgb(255, 255, 255);background-color: rgb(65, 109, 255);
-                }
-            """)
-            button.clicked.connect(self.buttonClicked)
-            scroll_layout.addWidget(button)
-
-        frame.setLayout(scroll_layout)
-
-        layout.addWidget(frame)
+        self.createCategoryButtons()
+        
 
 
     def button_entered6(self, event):
@@ -78,6 +50,144 @@ class LoveFrame(QDialog):
         print('BACK TO MAIN SCREEN')
         self.widget.setCurrentIndex(self.widget.currentIndex()-1)
 
-    def buttonClicked(self):
+
+    def createCategoryButtons(self):
+
+        # Create a list of image paths
+        response = requests.get('http://161.97.164.28:8080/api/categories/seemore')
+        data = json.loads(response.text)
+        button_list = data['content']
+
+        
+        scroll_layout = QVBoxLayout()
+        scroll_layout.setContentsMargins(15, 15, 15, 15)
+
+        
+        for i in button_list:
+            button= QPushButton(i['name'], self)
+            button.setStyleSheet("""
+                QPushButton {
+                    font: 12pt 'MS Shell Dlg 2';
+                    color: rgb(255, 255, 255);
+                    background-color:  qlineargradient(spread:pad, x1:0.211091, y1:0.199, x2:0.608, y2:0.603, stop:0.767045 rgba(30, 63, 102, 255), stop:0.977273 rgba(34, 70, 117, 255));
+                    
+                    border-radius: 20px;
+                }
+                QPushButton:hover {
+                    font: 12pt 'MS Shell Dlg 2';
+                    color: rgb(255, 255, 255);
+                    background-color: rgb(65, 109, 255);
+                }
+            """)
+            button.clicked.connect(partial(self.createBundlesButtons, i['_id']))
+            scroll_layout.addWidget(button)
+
+        self.widget.setLayout(scroll_layout)
+
+        # self.layout.addWidget(frame)
+
+    def createBundlesButtons(self, value):
+        # Create a list of image paths
+        url = 'http://161.97.164.28:8080/api/bundles/getbundles?categoryId=' + value
+        # print(url)
+        response = requests.get(url)
+        data = json.loads(response.text)
+        button_list = data['content']
+
+        # print(button_list)
+
+
+        scroll_layout = QVBoxLayout()
+        scroll_layout.setContentsMargins(15, 15, 15, 15)
+        scroll_layout.setSpacing(10)  # Adjust the spacing between buttons
+
+        
+        for i in button_list:
+            button= QPushButton(i['name'], self)
+            button.setStyleSheet("""
+                QPushButton {
+                    font: 12pt 'MS Shell Dlg 2';
+                    color: rgb(255, 255, 255);
+                    background-color:  qlineargradient(spread:pad, x1:0.211091, y1:0.199, x2:0.608, y2:0.603, stop:0.767045 rgba(30, 63, 102, 255), stop:0.977273 rgba(34, 70, 117, 255));
+                    
+                    border-radius: 20px;
+                }
+                QPushButton:hover {
+                    font: 12pt 'MS Shell Dlg 2';
+                    color: rgb(255, 255, 255);
+                    background-color: rgb(65, 109, 255);
+                }
+            """)
+            button.clicked.connect(partial(self.showImages, i['_id']))
+            scroll_layout.addWidget(button)
+
+
+        self.widget_2.setLayout(scroll_layout)
+
+    def showImages(self, value):
+        # Create a list of image paths
+        url = 'http://161.97.164.28:8080/api/frames/seemore?bundleId=' + value
+        # print(url)
+        response = requests.get(url)
+        data = json.loads(response.text)
+        images_data = data['content']
+        # print(images_data)
+
+        frame_width = 134  # Replace with your desired frame width
+        frame_height = 200  # Replace with your desired frame height
+
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+
+        scroll_content = QWidget()
+        scroll_area.setWidget(scroll_content)
+
+        layout = QVBoxLayout()
+        scroll_content.setLayout(layout)
+        
+        # Create a horizontal layout for displaying images in a row
+        h_layout = QHBoxLayout()
+
+        for image_url in images_data:
+            try:
+                response = requests.get(image_url['image'])
+                print('---->>', response.status_code)
+                if response.status_code == 200:
+                    image_data = BytesIO(response.content)
+                    # Convert BytesIO to bytes using getvalue()
+                    image_bytes = image_data.getvalue()
+                    pixmap = QPixmap()
+                    pixmap.loadFromData(image_bytes)
+
+                    # Scale the pixmap to the specified frame size
+                    pixmap = pixmap.scaled(frame_width, frame_height)
+
+                    if not pixmap.isNull():
+                        label = QLabel()
+                        label.setPixmap(pixmap)
+                        h_layout.addWidget(label)
+
+                        # Check if adding another label would exceed the available width
+                        if h_layout.sizeHint().width() > self.image_widget.viewport().width():
+                            layout.addLayout(h_layout)
+                            h_layout = QHBoxLayout()  # Start a new row
+                        
+                    # Add the last row to the layout
+                    layout.addLayout(h_layout)
+                else:
+                    print(f"-->Failed to load image from URL: {image_url['image']}, Status Code: {response.status_code}")
+            except Exception as e:
+                print(f"=>=>Failed to load image from URL: {image_url['image']}, Error: {str(e)}")
+
+                    
+        self.image_widget.setLayout(layout)
+
+
+
+
+
+
+    def buttonClick(self, value):
         sender = self.sender()  # Get the button that triggered the event
+        print('Value: ', value);
         print(f"Button clicked: {sender.text()}")
